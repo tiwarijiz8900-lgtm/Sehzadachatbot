397b62270abd389503f5eea60781c6333d97e440
𝐒𝐇𝐄𝐇𝐙𝐀𝐃𝐀 𝐅𝐔𝐂𝐊𝐄𝐑 <tiwarijiz8900@gmail.com>
2026-01-14 14:15:38 +0530

    ˹ sʜʀᴇᴇ ꭙ ϻᴜsɪᴄ ˼

---
+++ PRITI_CHATBOT/modules/helpers/__init__.py
@@ -0,0 +1,24 @@
+from typing import Callable
+
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.types import Message
+
+from PRITI_CHATBOT import OWNER, PRITI_CHATBOT
+
+
+def is_admins(func: Callable) -> Callable:
+    async def non_admin(c: PRITI_CHATBOT, m: Message):
+        if m.from_user.id == OWNER:
+            return await func(c, m)
+
+        admin = await c.get_chat_member(m.chat.id, m.from_user.id)
+        if admin.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+            return await func(c, m)
+
+    return non_admin
+
+
+from .inline import *
+from .read import *
+from .storage import *
+from .language import *
