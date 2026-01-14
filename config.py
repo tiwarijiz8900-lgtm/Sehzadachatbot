d6171707b7a424acc7020b0b5cad94ec50233a23
𝐒𝐇𝐄𝐇𝐙𝐀𝐃𝐀 𝐅𝐔𝐂𝐊𝐄𝐑 <tiwarijiz8900@gmail.com>
2026-01-14 14:27:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,15 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "25609334"
+API_ID = "32281986"
 # -------------------------------------------------------------
-API_HASH = "ad0ff353206ea1ebce1ab8bfca9f3f7b"
+API_HASH = "aaf868ca257794b6712868c1cc749ec8"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "8217013403"))
-SUPPORT_GRP = "LuckyXSupport"
-UPDATE_CHNL = "Luckyxupdate"
-OWNER_USERNAME = "The_LuckyX"
-LOGGER_GROUP = -1003213095654
+OWNER_ID = int(getenv("OWNER_ID", "8211189367"))
+SUPPORT_GRP = "Love_Ki_Duniyaa"
+UPDATE_CHNL = "Love_Bot_143"
+OWNER_USERNAME = "ll_WTF_SHEZADA_ll"
+LOGGER_GROUP = -1003605595874
